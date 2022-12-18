b9ecf91d0e9413c1a784b223e38639a91c35f290
Simran Preet Singh <simran5590@gmail.com>
2022-12-18 23:57:46 +0530

    Initial Commit

---
+++ Enum Navigation/ContentView.swift
@@ -0,0 +1,28 @@
+//
+// Created for Enum Navigation
+// by Stewart Lynch on 2022-10-30
+// Using Swift 5.0
+//
+// Follow me on Twitter: @StewartLynch
+// Subscribe on YouTube: https://youTube.com/StewartLynch
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
